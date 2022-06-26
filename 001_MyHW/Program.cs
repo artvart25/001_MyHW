46ce02c82edb852933bf194bdad5db989121185b
artvart25 <carlito77@bk.ru>
2022-06-26 15:46:59 +0300

    My first HW 001

---
+++ 001_MyHW/Program.cs
@@ -0,0 +1,24 @@
+﻿ int num1, num2;
+
+Console.Write("Input a first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("Max number is " + num1);
+}
+else
+{
+    Console.WriteLine("Max number is " + num2);
+}
+if(num1 < num2)
+{
+    Console.WriteLine("Min number is " + num1);
+}
+else
+{
+    Console.WriteLine("Min number is " + num2);
+}
