2f6bf21f3061f0b972c8aee53218075e4a14a9f1
artvart25 <carlito77@bk.ru>
2022-06-26 22:45:52 +0300

    Second commit 002 HW

--- 001_MyHW/Program.cs
+++ 001_MyHW/Program.cs
@@ -1,4 +1,9 @@
-﻿ int num1, num2;
+﻿
+
+/*
+ZADACHA 001 HW
+
+int num1, num2;
 
 Console.Write("Input a first number: ");
 num1 = Convert.ToInt32(Console.ReadLine());
@@ -22,3 +27,31 @@ else
 {
     Console.WriteLine("Min number is " + num2);
 }
+*/
+
+/*
+ZADACHA 002 HW
+
+int num1, num2, num3, maximum;
+
+Console.Write("Input a first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a third number: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+maximum=num1;
+
+if(num2>maximum)
+{
+    maximum=num2;
+}
+if(num3>maximum)
+{
+    maximum=num3;
+}
+Console.WriteLine("Max number is " + maximum);
+*/
