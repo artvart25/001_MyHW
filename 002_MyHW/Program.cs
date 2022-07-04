18d9af7116dbc3595aaf3bd61002344d341a5793
artvart25 <carlito77@bk.ru>
2022-07-04 14:13:26 +0300

    002 HW

--- 002_MyHW/Program.cs
+++ 002_MyHW/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int TwoNumber()
+{
+    int num;
+    Console.Write("Input a number: ");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    int one = num / 10;
+    int two = one % 10;
+
+    int result = two;
+    return result;
+}
+
+int num = TwoNumber();
+Console.WriteLine("Result is " + num);
+
+*/
+
+/*
+string ThirdNumber()
+{
+    Console.Write("Input a number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    string numText = Convert.ToString(num);
+    if (numText.Length > 2)
+    {
+        Console.WriteLine("Third number is " + numText[2]);
+    }
+    else 
+    {
+        Console.WriteLine("No third number");
+    }
+    return numText;
+}
+string number = ThirdNumber();
+
+*/
+
+/*
+void CheckingTheDayOfTheWeek () 
+{
+    Console.Write("Input a number of the day of the week ");
+    int dayNum = Convert.ToInt32(Console.ReadLine());
+
+    if (dayNum == 6 || dayNum == 7) 
+    {
+    Console.WriteLine("This day is holiday >> yes");
+    }
+    else 
+    if (dayNum < 1 || dayNum > 7) 
+    {
+    Console.WriteLine("This day is not a day of the week");
+    }
+    else 
+    {
+        Console.WriteLine("This day is holiday >> no");
+    }
+}
+CheckingTheDayOfTheWeek();
+*/
