22b29a05be7b6722204d466a3e427dbb02977afd
artvart25 <carlito77@bk.ru>
2022-07-05 14:10:58 +0300

    added 003_MyHW

--- 003_MyHW/Program.cs
+++ 003_MyHW/Program.cs
@@ -1,1 +1,47 @@
 ﻿
+/*
+void CheckZ(string number)
+{
+    if (number[0]==number[4] || number[1]==number[3]) 
+        Console.WriteLine("Число палиндром");
+    else 
+        Console.WriteLine("Число не палиндром");
+
+if (number!.Length > 5)
+    Console.WriteLine("Введите корректное число");
+}
+
+Console.Write("Input a number: ");
+string number = Console.ReadLine();
+
+CheckZ(number);
+*/
+
+/*
+void Cube(int[] cube)
+{
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  int index = 1;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+Console.Write("Input a number: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
+*/
