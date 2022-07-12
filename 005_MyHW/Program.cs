2e19070d187eff0931eef6394f837cb220bcae60
artvart25 <carlito77@bk.ru>
2022-07-12 16:04:41 +0300

    Added 005_MyHW

---
+++ 005_MyHW/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//массив random
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+    {
+        int[] newArray = new int[size];
+
+        for(int i = 0; i < size; i++)
+            newArray[i] = new Random().Next(minValue, maxValue + 1);
+        return newArray;
+    }
+
+//массив с вводом
+int[] CreateArray(int size)
+{
+    int newArray = new int[size];
+
+    Console.WriteLine("Creaying array here: ");
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} element: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Write();
+    return newArray;
+}
